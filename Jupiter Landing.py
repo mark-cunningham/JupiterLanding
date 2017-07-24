efb69f81601987352fbbc0fe634f7378cba70a42
mark-cunningham <cunniman13@gmail.com>
2017-07-24 16:14:47 +0100

    Pre beta

--- Jupiter Landing.py
+++ Jupiter Landing.py
@@ -1,9 +1,14 @@
-# Golf
+#!/usr/bin/python
+# Jupiter Landing
+# Code Angel
 
-import pygame, sys
+
+import sys
+import os
+import pygame
 from pygame.locals import *
-import math
 import random
+import math
 
 # Define the colours
 GREEN = (84, 216, 61)
@@ -21,46 +26,43 @@ MAXTERRAIN = 120
 MINTERRAIN = 90
 FUELWARNING = 50
 KGRAVITY = 0.01
+LANDER_START_Y = 40
 
+# Setup
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+pygame.init()
+game_screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+pygame.display.set_caption("Jupiter Landing")
+pygame.key.set_repeat(10, 20)
+clock = pygame.time.Clock()
+font = pygame.font.SysFont("Courier", 12)
 
+# Load images
+background_image = pygame.image.load("background.png").convert()
 
+planet_high_low_image = pygame.image.load("planet_high_low.png").convert_alpha()
+planet_high_mid_image = pygame.image.load("planet_high_mid.png").convert_alpha()
+planet_low_high_image = pygame.image.load("planet_low_high.png").convert_alpha()
+planet_low_mid_image = pygame.image.load("planet_low_mid.png").convert_alpha()
+planet_mid_image = pygame.image.load("planet_mid.png").convert_alpha()
+planet_mid_high_image = pygame.image.load("planet_mid_high.png").convert_alpha()
+planet_mid_low_image = pygame.image.load("planet_mid_low.png").convert_alpha()
+planet_blank_image = pygame.image.load("planet_blank.png").convert_alpha()
+planet_landing_block_image = pygame.image.load("planet_landing_block.png").convert_alpha()
 
+lander_image = pygame.image.load("lander.png").convert_alpha()
+boost_image = pygame.image.load("boost_lander.png").convert_alpha()
+exploded_lander_image = pygame.image.load("exploded_lander.png").convert_alpha()
 
 def main():
-    # Setup
-    pygame.init()
-    game_screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
-    pygame.display.set_caption("Jupiter Landing")
-    pygame.key.set_repeat(10, 20)
-    clock = pygame.time.Clock()
-    font = pygame.font.SysFont("Courier", 12)
-
-    # Load images
-    background_image = pygame.image.load("background.png").convert()
-
-    planet_high_low_image = pygame.image.load("planet_high_low.png").convert()
-    planet_high_mid_image = pygame.image.load("planet_high_mid.png").convert()
-    planet_low_high_image = pygame.image.load("planet_low_high.png").convert()
-    planet_low_mid_image = pygame.image.load("planet_low_mid.png").convert()
-    planet_mid_image = pygame.image.load("planet_mid.png").convert()
-    planet_mid_high_image = pygame.image.load("planet_mid_high.png").convert()
-    planet_mid_low_image = pygame.image.load("planet_mid_low.png").convert()
-    planet_blank_image = pygame.image.load("planet_blank.png").convert()
-    planet_landing_block_image = pygame.image.load("planet_landing_block.png").convert()
-
-    planet_images = {'HiLo': planet_high_low_image, 'HiMid': planet_high_mid_image, 'LoHi': planet_low_high_image,
-                     'LoMid': planet_low_mid_image, 'Mid': planet_mid_image, 'MidLo': planet_mid_low_image,
-                     'MidHi': planet_mid_high_image, 'Blank': planet_blank_image, 'Landing': planet_landing_block_image}
-                     
-
-    lander_image = pygame.image.load("lander.png").convert()
-    boost_image = pygame.image.load("boost_lander.png").convert()
-    exploded_lander_image = pygame.image.load("exploded_lander.png").convert()
-    display_lander_image = lander_image
 
     # Initialise variables
 
-    lander_center_y = 40
+
+
+    display_lander_image = lander_image
+
+    lander_center_y = LANDER_START_Y
     angle = 0
     velocity_x = 0
     velocity_y = 0
@@ -90,10 +92,9 @@ def main():
 
     
 
-    planet_dict = setup_planet(landing_blocks, planet_images)
+    planet_dict = setup_planet(landing_blocks)
     terrain = planet_dict['Terrain Blocks']
     terrain_y  = planet_dict['Terrain Blocks Y Coords']
-    terrain_name = planet_dict['Terrain Blocks Names']
     lander_center_x = planet_dict['Lander Start X']
     terrain_blocks = planet_dict['Number Terrain Blocks']
     landing_pad_block = planet_dict['Landing Pad']
@@ -165,7 +166,7 @@ def main():
                     
                     fuel = 500
 
-                lander_center_y = 40
+                lander_center_y = LANDER_START_Y
                 angle = 0
                 velocity_x = 0
                 velocity_y = 0
@@ -175,18 +176,17 @@ def main():
                 hit_terrain = False
                 landed = False
 
-                planet_dict = setup_planet(landing_blocks, planet_images)
+                planet_dict = setup_planet(landing_blocks)
                 terrain = planet_dict['Terrain Blocks']
                 terrain_y  = planet_dict['Terrain Blocks Y Coords']
-                tarrain_name = planet_dict['Terrain Blocks Names']
                 lander_center_x = planet_dict['Lander Start X']
                 terrain_blocks = planet_dict['Number Terrain Blocks']
                 landing_pad_block = planet_dict['Landing Pad']
        
 
-        if event.type == QUIT:
-            pygame.quit()     
-            sys.exit()
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
 
         
 
@@ -280,105 +280,112 @@ def main():
         # landed messages
         if hit_terrain is True:
             if landed is True:
-                message_text = "Congratulations Commander, a successful landing."
-                text = font.render(message_text, True, (GREEN))
-                
-                text_rect = text.get_rect()
-                game_screen.blit(text, [(SCREENWIDTH - text_rect.width) / 2, (SCREENHEIGHT - text_rect.height) / 2 - TEXTSPACE * 6])
-
+                display_successful_landing()
                 return_message_text = "Hit return for next level"
-
                 new_score = score + fuel * level + (landing_angle - abs(angle)) * 10 * level
                 
-                
-                
             else:
-                message_text = "Commander, you failed to land the spacecraft"
-                text = font.render(message_text, True, (GREEN))
-                text_rect = text.get_rect()
-                game_screen.blit(text, [(SCREENWIDTH - text_rect.width) / 2, (SCREENHEIGHT - text_rect.height) / 2 - TEXTSPACE * 6])
-                
-                fault_report_head = "Fault Report"
-                text = font.render(fault_report_head, True, (AMBER))
-                text_rect = text.get_rect()
-                game_screen.blit(text, [(SCREENWIDTH - text_rect.width) / 2, (SCREENHEIGHT - text_rect.height) / 2  - TEXTSPACE * 3])
-
+                display_failed_landing(on_screen, fuel_left, velocity_ok, landing_location_ok, angle_ok)
                 new_score = score
-
-                fault_text = ""
-                if on_screen is False:
-                    fault_text = fault_text + "Left planet orbit. "
-                else:
-                    if fuel_left is False:
-                        fault_text = fault_text + "No fuel. "
-                    if velocity_ok is False:
-                        fault_text = fault_text + "Approach velocity too high. "    
-                    if landing_location_ok is False:
-                        fault_text = fault_text + "Missed landing pad. "
-                    if angle_ok is False:
-                        fault_text = fault_text + "Approach angle too high. "
-                    
-
-                text = font.render(fault_text, True, (AMBER))
-                text_rect = text.get_rect()
-                game_screen.blit(text, [(SCREENWIDTH - text_rect.width) / 2, (SCREENHEIGHT - text_rect.height) / 2 - TEXTSPACE * 2])
-                
                 return_message_text = "Hit return for new game"
 
-            score_text = "Your overall score is " + str(new_score)
-            text = font.render(score_text, True, (GREEN))
-            text_rect = text.get_rect()
-            game_screen.blit(text, [(SCREENWIDTH - text_rect.width) / 2, (SCREENHEIGHT - text_rect.height) / 2  - TEXTSPACE * 5])
-
-            text = font.render(return_message_text, True, (GREEN))
-            text_rect = text.get_rect()
-            game_screen.blit(text, [(SCREENWIDTH - text_rect.width) / 2, (SCREENHEIGHT - text_rect.height) / 2 + TEXTSPACE * 1])   
+            display_game_end(new_score, return_message_text)
 
         else:
-            # display status
-            fuel_text = "Fuel: " + str(fuel)
-            if fuel < FUELWARNING:
-                text = font.render(fuel_text, True, (AMBER))
-            else:
-                text = font.render(fuel_text, True, (GREEN))
-            game_screen.blit(text, [LEFTMARGIN, TEXTSPACE * 2])
-
-
-            h_velocity_text = "H Vel: " + str(round(velocity_x, 2))
-            if abs(velocity_x) > landing_velocity:
-                text = font.render(h_velocity_text, True, (AMBER))
-            else:
-                text = font.render(h_velocity_text, True, (GREEN))
-            game_screen.blit(text, [LEFTMARGIN, TEXTSPACE * 3])
-            
-            v_velocity_text = "V Vel: " + str(round(velocity_y, 2))
-            if velocity_y > landing_velocity: 
-                text = font.render(v_velocity_text, True, (AMBER))
-            else:
-                text = font.render(v_velocity_text, True, (GREEN))
-                
-            game_screen.blit(text, [LEFTMARGIN, TEXTSPACE * 4])
-
-            angle_text = "Angle: " + str(-angle)
-            if abs(angle) > landing_angle:
-                text = font.render(angle_text, True, (AMBER))
-            else:
-                text = font.render(angle_text, True, (GREEN))
-            game_screen.blit(text, [LEFTMARGIN, TEXTSPACE * 5])
-
-            level_text = "Level: " + str(level)
-            text = font.render(level_text, True, (GREEN))
-            game_screen.blit(text, [LEFTMARGIN, TEXTSPACE * 6])
-
-        
-               
-
+            display_status(fuel, velocity_x, landing_velocity, velocity_y, angle, landing_angle, level)
 
 
         pygame.display.update()
         clock.tick(60)
 
-def setup_planet(landing_blocks, planet_images):
+def display_successful_landing():
+    message_text = "Congratulations Commander, a successful landing."
+    text = font.render(message_text, True, (GREEN))
+
+    text_rect = text.get_rect()
+    game_screen.blit(text, [(SCREENWIDTH - text_rect.width) / 2, (SCREENHEIGHT - text_rect.height) / 2 - TEXTSPACE * 6])
+
+
+
+def display_failed_landing(on_screen, fuel_left, velocity_ok, landing_location_ok, angle_ok):
+    message_text = "Commander, you failed to land the spacecraft"
+    text = font.render(message_text, True, (GREEN))
+    text_rect = text.get_rect()
+    game_screen.blit(text, [(SCREENWIDTH - text_rect.width) / 2, (SCREENHEIGHT - text_rect.height) / 2 - TEXTSPACE * 6])
+
+    fault_report_head = "Fault Report"
+    text = font.render(fault_report_head, True, (AMBER))
+    text_rect = text.get_rect()
+    game_screen.blit(text, [(SCREENWIDTH - text_rect.width) / 2, (SCREENHEIGHT - text_rect.height) / 2 - TEXTSPACE * 3])
+
+    fault_text = ""
+    if on_screen is False:
+        fault_text = fault_text + "Left planet orbit. "
+    else:
+        if fuel_left is False:
+            fault_text = fault_text + "No fuel. "
+        if velocity_ok is False:
+            fault_text = fault_text + "Approach velocity too high. "
+        if landing_location_ok is False:
+            fault_text = fault_text + "Missed landing pad. "
+        if angle_ok is False:
+            fault_text = fault_text + "Approach angle too high. "
+
+    text = font.render(fault_text, True, (AMBER))
+    text_rect = text.get_rect()
+    game_screen.blit(text, [(SCREENWIDTH - text_rect.width) / 2, (SCREENHEIGHT - text_rect.height) / 2 - TEXTSPACE * 2])
+
+def display_game_end(score, message):
+    score_text = "Your overall score is " + str(score)
+    text = font.render(score_text, True, (GREEN))
+    text_rect = text.get_rect()
+    game_screen.blit(text, [(SCREENWIDTH - text_rect.width) / 2, (SCREENHEIGHT - text_rect.height) / 2 - TEXTSPACE * 5])
+
+    text = font.render(message, True, (GREEN))
+    text_rect = text.get_rect()
+    game_screen.blit(text, [(SCREENWIDTH - text_rect.width) / 2, (SCREENHEIGHT - text_rect.height) / 2 + TEXTSPACE * 1])
+
+def display_status(fuel, velocity_x, landing_velocity, velocity_y, angle, landing_angle, level):
+    # display status
+    fuel_text = "Fuel: " + str(fuel)
+    if fuel < FUELWARNING:
+        text = font.render(fuel_text, True, (AMBER))
+    else:
+        text = font.render(fuel_text, True, (GREEN))
+    game_screen.blit(text, [LEFTMARGIN, TEXTSPACE * 2])
+
+    h_velocity_text = "H Vel: " + str(round(velocity_x, 2))
+    if abs(velocity_x) > landing_velocity:
+        text = font.render(h_velocity_text, True, (AMBER))
+    else:
+        text = font.render(h_velocity_text, True, (GREEN))
+    game_screen.blit(text, [LEFTMARGIN, TEXTSPACE * 3])
+
+    v_velocity_text = "V Vel: " + str(round(velocity_y, 2))
+    if velocity_y > landing_velocity:
+        text = font.render(v_velocity_text, True, (AMBER))
+    else:
+        text = font.render(v_velocity_text, True, (GREEN))
+
+    game_screen.blit(text, [LEFTMARGIN, TEXTSPACE * 4])
+
+    angle_text = "Angle: " + str(-angle)
+    if abs(angle) > landing_angle:
+        text = font.render(angle_text, True, (AMBER))
+    else:
+        text = font.render(angle_text, True, (GREEN))
+    game_screen.blit(text, [LEFTMARGIN, TEXTSPACE * 5])
+
+    level_text = "Level: " + str(level)
+    text = font.render(level_text, True, (GREEN))
+    game_screen.blit(text, [LEFTMARGIN, TEXTSPACE * 6])
+
+
+def setup_planet(landing_blocks):
+
+    planet_images = {'HiLo': planet_high_low_image, 'HiMid': planet_high_mid_image, 'LoHi': planet_low_high_image,
+                     'LoMid': planet_low_mid_image, 'Mid': planet_mid_image, 'MidLo': planet_mid_low_image,
+                     'MidHi': planet_mid_high_image, 'Blank': planet_blank_image, 'Landing': planet_landing_block_image}
     
     terrain = []
     terrain_y = []
@@ -424,36 +431,36 @@ def setup_planet(landing_blocks, planet_images):
             if last_block == "mid":
                 if next_block == "mid":
                     terrain_choice = planet_images['Mid']
-                    terrain_name.append("mid")
+                    #terrain_name.append("mid")
                 elif next_block == "higher":
                     terrain_choice = planet_images['MidHi']
-                    terrain_name.append("mid_high")
+                    #terrain_name.append("mid_high")
                 elif next_block == "lower":
                     terrain_choice = planet_images['MidLo']
-                    terrain_name.append("mid_low")
+                    #terrain_name.append("mid_low")
                 
             elif last_block == "higher":
                 if next_block == "mid":
                     terrain_choice = planet_images['HiMid']
-                    terrain_name.append("high_mid")
+                    #terrain_name.append("high_mid")
                 elif next_block == "higher":
                     terrain_choice = planet_images['LoHi']
-                    terrain_name.append("low_high")
+                    #terrain_name.append("low_high")
                     terrain_y_coord = terrain_y_coord - 1
                 elif next_block == "lower":
                     terrain_choice = planet_images['HiLo']
-                    terrain_name.append("high_low")
+                    #terrain_name.append("high_low")
                 
             elif last_block == "lower":
                 if next_block == "mid":
                     terrain_choice = planet_images['LoMid']
-                    terrain_name.append("low_mid")
+                    #terrain_name.append("low_mid")
                 elif next_block == "higher":
                     terrain_choice = planet_images['LoHi']
-                    terrain_name.append("low_high")
+                    #terrain_name.append("low_high")
                 elif next_block == "lower":
                     terrain_choice = planet_images['HiLo']
-                    terrain_name.append("high_low")
+                    #terrain_name.append("high_low")
                     terrain_y_coord = terrain_y_coord + 1
 
         if terrain_y_coord > MAXTERRAIN - 1:
@@ -476,9 +483,10 @@ def setup_planet(landing_blocks, planet_images):
 
         last_block = next_block
 
-    planet_dict = {'Terrain Blocks': terrain, 'Terrain Blocks Y Coords': terrain_y, 'Terrain Blocks Names': terrain_name, 'Lander Start X': lander_center_x, 'Number Terrain Blocks': terrain_blocks, 'Landing Pad': landing_pad_block}
+    planet_dict = {'Terrain Blocks': terrain, 'Terrain Blocks Y Coords': terrain_y, 'Lander Start X': lander_center_x, 'Number Terrain Blocks': terrain_blocks, 'Landing Pad': landing_pad_block}
     return planet_dict
 
 
 if __name__ == "__main__":
+
     main()
